40aa31e2d04a68fd2d9fd749e564efb9ef563993
fuxin3 <fuxin@singleronbio.com>
2022-08-16 15:55:35 +0800

    test

--- test/run_pacbio.sh
+++ test/run_pacbio.sh
@@ -1,3 +1,3 @@
 python ../src/run_pacbio.py --sample test_sample --outdir test_res_all/ --ccs_bam /SGRNJ03/randd/user/fuxin/Github_repo/Pacbio_Analysis/test/test.bam --bu_pattern T-12U-57B --steps all
 
-python ../src/run_pacbio.py --sample test_sample --outdir test_res_isoform/ --ccs_bam /SGRNJ03/randd/user/fuxin/Github_repo/Pacbio_Analysis/test/test.bam --bu_pattern T-12U-57B --steps ccs,remove.primers,pattern.bc.umi,flc,split.linker,dedup,run_isoform
+python ../src/run_pacbio.py --sample test_sample --outdir test_res_isoform/ --ccs_bam /SGRNJ03/randd/user/fuxin/Github_repo/Pacbio_Analysis/test/test.bam --bu_pattern T-12U-57B --steps ccs,remove_primer,pattern_detection,flc,split_linker,dedup,run_isoform
