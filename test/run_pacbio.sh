e297d257b54f692392af7cac728c055b0ac4ad27
fuxin3 <fuxin@singleronbio.com>
2023-05-10 17:33:05 +0800

    update test sh

--- test/run_pacbio.sh
+++ test/run_pacbio.sh
@@ -1,3 +1,5 @@
+python ../src/run_pacbio.py --sample test_sample --outdir test_res_all_1 --ccs_bam /SGRNJ03/randd/user/fuxin/Github_repo/Pacbio_Analysis/test/test.bam --bu_pattern T-12U-57B --pacbio_source_path /SGRNJ03/randd/user/fuxin/Github_repo/dev/Pacbio_Analysis --steps run_isoform
+
 python ../src/run_pacbio.py --sample test_sample --outdir test_res_all/ --ccs_bam /SGRNJ03/randd/user/fuxin/Github_repo/Pacbio_Analysis/test/test.bam --bu_pattern T-12U-57B --steps all
 
 python ../src/run_pacbio.py --sample test_sample --outdir test_res_isoform/ --ccs_bam /SGRNJ03/randd/user/fuxin/Github_repo/Pacbio_Analysis/test/test.bam --bu_pattern T-12U-57B --steps ccs,remove_primer,pattern_detection,flc,split_linker,dedup,run_isoform
